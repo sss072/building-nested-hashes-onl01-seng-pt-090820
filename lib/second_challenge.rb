246c2c4d0fa842640ad48a64f9e946a5fbc2702c
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-27 04:44:59 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,14 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {patriarch: {}, matriarch: {}, hero: {}, hero_friends: []}, 
-    :capulet => {patriarch: {}, matriarch: {}, hero: {}, hero_friends: []}}
-
-  
-end
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
